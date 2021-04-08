c52696b6a4820693ff1553a55ffbc42df38230b2
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-04-08 02:04:58 +0200

    Added sqlxes package database migration tools

---
+++ eventsource/sqlxes/migrate.go
@@ -0,0 +1,71 @@
+package sqlxes
+
+import (
+	"bytes"
+	_ "embed"
+	"errors"
+	"strings"
+	"text/template"
+
+	"github.com/jmoiron/sqlx"
+
+	"github.com/kucjac/cleango/xlog"
+)
+
+//go:embed mysql.tmpl
+var mysqlMigrateQuery string
+
+//go:embed mysql.tmpl
+var postgresMysqlQuery string
+
+var (
+	migrateMySQL    *template.Template
+	migratePostgres *template.Template
+)
+
+func init() {
+	t := template.New("")
+	migrateMySQL = template.Must(t.Parse(mysqlMigrateQuery))
+	migratePostgres = template.Must(t.Parse(postgresMysqlQuery))
+}
+
+// Migrate executes table and types migration for the event store and snapshot.
+// The table names are taken from the config.
+func Migrate(config *Config, conn *sqlx.DB) error {
+	var buf bytes.Buffer
+	switch conn.DriverName() {
+	case "pg", "postgres", "postgresql", "gopg", "pgx":
+		if err := migratePostgres.Execute(&buf, config); err != nil {
+			return err
+		}
+	case "mysql":
+		if err := migrateMySQL.Execute(&buf, config); err != nil {
+			return err
+		}
+	default:
+		return errors.New("driver not supported by the sqlxes migration tool")
+	}
+
+	commands := strings.Split(buf.String(), ";")
+	var resCmds []string
+	for _, cmd := range commands {
+		cmd = strings.TrimSpace(cmd)
+		if cmd != "" {
+			resCmds = append(resCmds, cmd)
+		}
+	}
+
+	tx, err := conn.Begin()
+	if err != nil {
+		return err
+	}
+	for _, cmd := range resCmds {
+		xlog.Debug(cmd)
+		if _, err = tx.Exec(cmd); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	tx.Commit()
+	return nil
+}
