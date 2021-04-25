7cacb2b7d2142b5e36bf45eb2f83342eaf4391ad
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-04-25 02:19:40 +0200

    Fixed pubsub gen.go destination.

--- pubsub/gen.go
+++ pubsub/gen.go
@@ -2,4 +2,4 @@ package pubsub
 
 import _ "github.com/golang/mock/mockgen/model"
 
-//go:generate mockgen -destination=./../mockps/bus.go -package=mockps . Publisher,Subscriber
+//go:generate mockgen -destination=./mockps/bus.go -package=mockps . Publisher,Subscriber
