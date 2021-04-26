cc3886bbe51a6ec224b385d3869f884860f645de
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-04-26 16:14:18 +0200

    Provided pubsub/codec ProtoMarshaler and ProtoUnmarshaler interfaces.

--- pubsub/codec/proto.go
+++ pubsub/codec/proto.go
@@ -13,10 +13,23 @@ func Proto() Codec {
 
 var _ Codec = (*protoCodec)(nil)
 
+// ProtoMarshaler is an interface used by the protobuf codec used to custom marshal proto message.
+type ProtoMarshaler interface {
+	MarshalProto() ([]byte, error)
+}
+
+// ProtoUnmarshaler is an interface used for custom unmarshalling proto message.
+type ProtoUnmarshaler interface {
+	UnmarshalProto([]byte) error
+}
+
 type protoCodec struct{}
 
 // Marshal implements codec.Marshaler interface.
 func (j *protoCodec) Marshal(input interface{}) ([]byte, error) {
+	if mp, ok := input.(ProtoMarshaler); ok {
+		return mp.MarshalProto()
+	}
 	msg, ok := input.(proto.Message)
 	if !ok {
 		return nil, errors.New("protobuf input is not a proto.Message")
@@ -26,6 +39,10 @@ func (j *protoCodec) Marshal(input interface{}) ([]byte, error) {
 
 // Unmarshal implements codec.Marshaler interface
 func (j *protoCodec) Unmarshal(data []byte, output interface{}) error {
+	up, ok := output.(ProtoUnmarshaler)
+	if ok {
+		return up.UnmarshalProto(data)
+	}
 	msg, ok := output.(proto.Message)
 	if !ok {
 		return errors.New("protobuf output is not a proto.Message")
