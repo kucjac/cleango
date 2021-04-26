6ae6c48cc4112aeee6ecc63e80b52de5323b34b3
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-04-26 12:05:26 +0200

    Changed pubsub.Message to pubsub.MessageTopicer interface name. Minor changes in pubsub/mockps.

--- pubsub/message.go
+++ pubsub/message.go
@@ -1,6 +1,6 @@
 package pubsub
 
-// Message is the event message interface.
-type Message interface {
+// MessageTopicer is the interface used for the messages that has it's topic defined and returned in the MessageTopic method.
+type MessageTopicer interface {
 	MessageTopic() string
 }
