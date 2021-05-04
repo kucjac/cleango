23fbc814d9e10f3b2c382e21963e5c4097d6b353
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-05-04 18:43:52 +0200

    Provided xservice RunService function.

---
+++ xservice/listen.go
@@ -0,0 +1,53 @@
+package xservice
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/kucjac/cleango/cgerrors"
+	"github.com/kucjac/cleango/xlog"
+)
+
+// RunService runs the service and locks the thread while running given service.
+func RunService(ctx context.Context, s RunnerCloser) error {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGABRT, syscall.SIGINT, syscall.SIGTERM)
+
+	ctx, cancel := context.WithCancel(ctx)
+	errorChan := make(chan error, 1)
+
+	// Run the service.
+	go func(s Runner, cancel context.CancelFunc) {
+		var err error
+		if err = s.Run(); err != nil {
+			if cgerrors.Code(err) != cgerrors.ErrorCode_Canceled {
+				xlog.Errorf("ListenAndServe failed: %v", err)
+				errorChan <- err
+			} else {
+				cancel()
+			}
+		}
+	}(s, cancel)
+
+	select {
+	case <-ctx.Done():
+		xlog.Infof("Service context had finished.")
+	case sig := <-quit:
+		xlog.Infof("Received Signal: '%s'. Shutdown Server begins...", sig.String())
+	case err := <-errorChan:
+		// The error from the server running.
+		return err
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second*20)
+	defer cancel()
+	if err := s.Close(); err != nil {
+		xlog.Errorf("Server shutdown failed: %v", err)
+		return err
+	}
+	xlog.Info("Server had shutdown successfully.")
+	return nil
+}
