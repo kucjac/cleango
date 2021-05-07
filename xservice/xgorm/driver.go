9a8f09b5efc8196cd317ffacbcd7410103b74cd3
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-05-07 18:02:12 +0200

    created new xgorm driver.

---
+++ xservice/xgorm/driver.go
@@ -0,0 +1,40 @@
+package xgorm
+
+import (
+	"errors"
+
+	"github.com/kucjac/cleango/cgerrors"
+	"github.com/kucjac/cleango/xservice"
+	"gorm.io/gorm"
+)
+
+var _ xservice.Driver = (*driver)(nil)
+
+// New creates a new gorm driver.
+func New(baseDriver xservice.Driver) (xservice.Driver, error) {
+	if baseDriver == nil {
+		return nil, errors.New("xgorm provided nil base driver")
+	}
+	return &driver{base: baseDriver}, nil
+}
+
+type driver struct {
+	base xservice.Driver
+}
+
+func (d *driver) ErrorCode(err error) cgerrors.ErrorCode {
+	switch err {
+	case gorm.ErrRecordNotFound:
+		return cgerrors.ErrorCode_NotFound
+	case gorm.ErrInvalidTransaction, gorm.ErrNotImplemented, gorm.ErrMissingWhereClause, gorm.ErrUnsupportedRelation, gorm.ErrPrimaryKeyRequired, gorm.ErrModelValueRequired, gorm.ErrUnsupportedDriver, gorm.ErrDryRunModeUnsupported, gorm.ErrInvalidDB, gorm.ErrRegistered, gorm.ErrInvalidField:
+		return cgerrors.ErrorCode_Internal
+	case gorm.ErrEmptySlice, gorm.ErrInvalidData, gorm.ErrInvalidValue, gorm.ErrInvalidValueOfLength:
+		return cgerrors.ErrorCode_InvalidArgument
+	default:
+		return d.base.ErrorCode(err)
+	}
+}
+
+func (d *driver) CanRetry(err error) bool {
+	return d.base.CanRetry(err)
+}
---
+++ xservice/xgorm/go.mod
@@ -0,0 +1,8 @@
+module github.com/kucjac/cleango/xservice/xgorm
+
+go 1.16
+
+require (
+	github.com/kucjac/cleango v0.0.0-20210507153752-cb1818c41bf4
+	gorm.io/gorm v1.21.9
+)
