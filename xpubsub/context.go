bcf8e162803e6d97f7ce973f757ef3b998ce7da4
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-06-21 19:29:20 +0200

    xpubsub: renamed context key names.

--- xpubsub/context.go
+++ xpubsub/context.go
@@ -3,8 +3,8 @@ package xpubsub
 type subscriptionCtx string
 
 var (
-	// SubTopicCtxKey is the key for the subscription topic.
-	SubTopicCtxKey = subscriptionCtx("sub:topic")
-	// SubIdCtxKey is the key for the subscription id.
-	SubIdCtxKey = subscriptionCtx("sub:id")
+	// SubscriptionSubjectCtxKey is the key for the subscription topic.
+	SubscriptionSubjectCtxKey = subscriptionCtx("sub:topic")
+	// SubscriptionIdCtxKey is the key for the subscription id.
+	SubscriptionIdCtxKey = subscriptionCtx("sub:id")
 )
