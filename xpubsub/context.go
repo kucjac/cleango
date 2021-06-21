66204a8b532df4d34f9fbc3e36187ad844aa1edb
Jacek Kucharczyk <jacek@smartlycode.pl>
2021-06-21 19:31:02 +0200

    xpubsub/pubsubmiddleware/natsmiddleware: Implemented module and first middleware - MsgSubject.

--- xpubsub/context.go
+++ xpubsub/context.go
@@ -1,10 +1,41 @@
 package xpubsub
 
+import (
+	"context"
+	"time"
+)
+
 type subscriptionCtx string
 
 var (
 	// SubscriptionSubjectCtxKey is the key for the subscription topic.
-	SubscriptionSubjectCtxKey = subscriptionCtx("sub:topic")
+	SubscriptionSubjectCtxKey = subscriptionCtx("pubsub:subject")
 	// SubscriptionIdCtxKey is the key for the subscription id.
-	SubscriptionIdCtxKey = subscriptionCtx("sub:id")
+	SubscriptionIdCtxKey = subscriptionCtx("pubsub:id")
+	// MessageTimestampKey is the key for the message timestamp.
+	MessageTimestampKey = subscriptionCtx("pubsub:msg.timestamp")
+	// MessageSequenceKey is the key for the message timestamp.
+	MessageSequenceKey = subscriptionCtx("pubsub:msg.sequence")
 )
+
+// CtxSetMessageSequence sets the message sequence in the context,
+func CtxSetMessageSequence(ctx context.Context, seq int64) context.Context {
+	return context.WithValue(ctx, MessageSequenceKey, seq)
+}
+
+// CtxGetMessageSequence gets the message sequence from the context.
+func CtxGetMessageSequence(ctx context.Context) (int64, bool) {
+	v, ok := ctx.Value(MessageSequenceKey).(int64)
+	return v, ok
+}
+
+// CtxSetMessageTimestamp sets the message timestamp in the context.
+func CtxSetMessageTimestamp(ctx context.Context, timestamp time.Time) context.Context {
+	return context.WithValue(ctx, MessageTimestampKey, timestamp)
+}
+
+// CtxGetMessageTimestamp gets the message timestamp from the context.
+func CtxGetMessageTimestamp(ctx context.Context) (time.Time, bool) {
+	v, ok := ctx.Value(MessageTimestampKey).(time.Time)
+	return v, ok
+}
---
+++ xpubsub/pubsubmiddleware/natsmiddleware/message_subject.go
@@ -0,0 +1,138 @@
+package natsmiddleware
+
+import (
+	"context"
+
+	"github.com/nats-io/nats.go"
+	"gocloud.dev/pubsub"
+	_ "gocloud.dev/pubsub/natspubsub"
+
+	"github.com/kucjac/cleango/cgerrors"
+	"github.com/kucjac/cleango/metadata"
+	"github.com/kucjac/cleango/xpubsub"
+)
+
+// Compile time check if the MsgSubject is a xpubsub.Middleware.
+var _ xpubsub.Middleware = MsgSubject
+
+// MsgSubject is a xpubsub.Middleware that is extracting subject from the subscription message.
+func MsgSubject(next xpubsub.Handler) xpubsub.Handler {
+	return xpubsub.HandlerFunc(func(ctx context.Context, msg *pubsub.Message) error {
+		var natsMsg *nats.Msg
+		if !msg.As(&natsMsg) {
+			return cgerrors.ErrInternal("pubsub.Message.As returned failed").
+				WithMeta("msgId", msg.LoggableID)
+		}
+
+		// Get the metadata from the context.
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			meta = metadata.Meta{}
+			ctx = metadata.NewContext(ctx, meta)
+		}
+		// Store the subject in the metadata.
+		meta.Set(metadata.KeySubject, natsMsg.Subject)
+
+		// Handle the next handlers.
+		if err := next.Handle(ctx, msg); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// JetStreamMetadataCtxKey is the key to the JetStream Message metadata.
+type JetStreamMetadataCtxKey struct{}
+
+// Compile time check if the ExtractJetStreamMetadata function is xpubsub.Middleware.
+var _ xpubsub.Middleware = ExtractJetStreamMetadata
+
+// ExtractJetStreamMetadata is a middleware that extracts JetStream message metadata and stores in the context
+// with the key JetStreamMetadataCtxKey.
+func ExtractJetStreamMetadata(next xpubsub.Handler) xpubsub.Handler {
+	return xpubsub.HandlerFunc(func(ctx context.Context, msg *pubsub.Message) error {
+		var natsMsg *nats.Msg
+		if !msg.As(&natsMsg) {
+			return cgerrors.ErrInternal("pubsub.Message.As returned failed").
+				WithMeta("msgId", msg.LoggableID)
+		}
+		// Get JetStream metadata.
+		meta, err := natsMsg.Metadata()
+		if err != nil {
+			return err
+		}
+
+		// Set up the timestamp in the context.
+		ctx = context.WithValue(ctx, JetStreamMetadataCtxKey{}, meta)
+
+		// Handle the next handlers.
+		if err := next.Handle(ctx, msg); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// Compile time check if the ExtractJetStreamMsgTimestamp function is xpubsub.Middleware.
+var _ xpubsub.Middleware = ExtractJetStreamMsgTimestamp
+
+// ExtractJetStreamMsgTimestamp extracts the timestamp from the JetStream Message metadata and stores it in the context.
+func ExtractJetStreamMsgTimestamp(next xpubsub.Handler) xpubsub.Handler {
+	return xpubsub.HandlerFunc(func(ctx context.Context, msg *pubsub.Message) error {
+		meta, ok := ctx.Value(JetStreamMetadataCtxKey{}).(*nats.MsgMetadata)
+		if !ok {
+			var natsMsg *nats.Msg
+			if !msg.As(&natsMsg) {
+				return cgerrors.ErrInternal("pubsub.Message.As returned failed").
+					WithMeta("msgId", msg.LoggableID)
+			}
+			// Get JetStream metadata.
+			var err error
+			meta, err = natsMsg.Metadata()
+			if err != nil {
+				return err
+			}
+		}
+
+		// Set up the timestamp in the context.
+		ctx = xpubsub.CtxSetMessageTimestamp(ctx, meta.Timestamp)
+
+		// Handle the next handlers.
+		if err := next.Handle(ctx, msg); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// Compile time check if the ExtractJetStreamMsgSequence function is xpubsub.Middleware.
+var _ xpubsub.Middleware = ExtractJetStreamMsgSequence
+
+// ExtractJetStreamMsgSequence extracts the timestamp from the JetStream Message metadata and stores it in the context.
+func ExtractJetStreamMsgSequence(next xpubsub.Handler) xpubsub.Handler {
+	return xpubsub.HandlerFunc(func(ctx context.Context, msg *pubsub.Message) error {
+		meta, ok := ctx.Value(JetStreamMetadataCtxKey{}).(*nats.MsgMetadata)
+		if !ok {
+			var natsMsg *nats.Msg
+			if !msg.As(&natsMsg) {
+				return cgerrors.ErrInternal("pubsub.Message.As returned failed").
+					WithMeta("msgId", msg.LoggableID)
+			}
+			// Get JetStream metadata.
+			var err error
+			meta, err = natsMsg.Metadata()
+			if err != nil {
+				return err
+			}
+		}
+
+		// Set up the timestamp in the context.
+		ctx = xpubsub.CtxSetMessageSequence(ctx, int64(meta.Sequence.Stream))
+
+		// Handle the next handlers.
+		if err := next.Handle(ctx, msg); err != nil {
+			return err
+		}
+		return nil
+	})
+}
